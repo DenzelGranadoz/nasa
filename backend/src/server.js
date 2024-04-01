a58b0d4db31f73fd640d00084a6e9a62787ccee3
Denzel Granadoz <zelgranadoz@gmail.com>
2024-04-01 21:06:14 +0800

    .env file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,12 @@
 const http = require('http');
 const mongoose = require('mongoose');
 const app = require('./app');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
-
-const MONGO_URL =
-  'mongodb+srv://zel:admin12345_@nasacluster.zgf21t6.mongodb.net/?retryWrites=true&w=majority&appName=NASAcluster';
 
 const server = http.createServer(app);
 
@@ -19,7 +18,7 @@ mongoose.connection.on('error', (err) => {
 });
 
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(process.env.MONGO_URL);
 
   await loadPlanetsData();
 
