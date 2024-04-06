0b0f8db33adccc46a4ffcddd414e23cef7961ce3
Denzel Granadoz <zelgranadoz@gmail.com>
2024-04-06 09:18:07 +0800

    updated test cases for mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,15 @@
 const http = require('http');
-const mongoose = require('mongoose');
 const app = require('./app');
-const dotenv = require('dotenv');
-dotenv.config();
 
 const { loadPlanetsData } = require('./models/planets.model');
+const { mongoConnect } = require('./services/mongo');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection ready');
-});
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(process.env.MONGO_URL);
-
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
